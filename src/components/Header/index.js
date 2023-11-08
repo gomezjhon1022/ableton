6468959cda6007dfdc7b78b49cb7b620a400bcb5
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-11-08 09:09:52 +0100

    Feat: Add 'More' button funtionality

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,11 @@ import { textHeader } from '../../assets/text/textHeader';
 import './Header.scss';
 
 function Header(){
-  const [language, setLanguage]=useState('es');
+  const [language, setLanguage]=useState('en');
   const textList = textHeader[language];
-  console.log(textList);
-  console.log(textList.header)
   const [isExpanded, setIsExpanded] =useState(false);
   const [isDesktop, setIsDesktop]=useState(false);
-
+  const [isExpandedButtonMore, setIsExpandedButtonMore]=useState(false);
   const handleMenu=()=>{
     setIsExpanded(!isExpanded);
   }
@@ -25,6 +23,10 @@ function Header(){
       window.removeEventListener('resize', handleResize);
     };
   },[]);
+
+  const handleButtonPlus = () => {
+    setIsExpandedButtonMore(!isExpandedButtonMore);
+  }
 return (
   <header className={`header ${isExpanded===true?'is-expanded':''}`}>
     <div className='header--wrapper'>
@@ -32,7 +34,22 @@ return (
         <a className='header__logo'></a>
         {isDesktop?
             <ul className='header__nav-links-desktop'>
-              {textList.header.map((element)=>(<li className='header__link-desktop' key={element}><a>{element}</a></li>))}
+              {textList.header.map((element,index)=>(
+                index ===7?
+                <li className='header__link-desktop' key={element}>
+                  <button className='header__buttonMore' onClick={()=>handleButtonPlus()}>
+                    <span>{element}</span>
+                    <span className='header__iconPlus' >
+                      {isExpandedButtonMore?
+                        <span className='header__iconPlus-expanded'></span>
+                        :<span className='header__iconPlus-collapse'></span>
+                      }
+                    </span>
+                  </button>
+                </li>
+                :
+                <li className='header__link-desktop' key={element}><a>{element}</a></li>
+                ))}
             </ul>
           :<>
             <button className='header--trigger-label' onClick={()=>handleMenu()}>Menu</button>
