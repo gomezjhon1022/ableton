31e6291471334dd3668f75b64321737efc44b18f
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-11-29 09:16:26 +0100

    Chore: Add Spanish language support to the main home section

---
+++ src/assets/text/textHome.js
@@ -0,0 +1,60 @@
+const textHome =
+{ "en" : {
+    "links": [
+      "We make ",
+      "Live",
+      "Push",
+      " and " ,
+      "Link"
+    ],
+    "title": [
+      " - unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.",
+      "Making music isn't easy. It takes time, effort, and learning. But when you're in the flow, it's incredibly rewarding.",
+      "We are more than 350 people from 30 different countries divided between our headquarters in Berlin and our offices in Los Angeles and Tokyo.",
+      "We believe it takes focus to create truly outstanding instruments. We only work on a few products and we strive to make them great.",
+      "We're passionate about what we do, but we're equally passionate about improving who we are.",
+      "We want our employees to love it here. Since we're looking for exceptional talent from around the world, we will do everything we can to make your transition as easy as possible."
+
+      ],
+    "paragraph": [
+      "Ableton was founded in 1999 and released the first version of Live in 2001. Our products are used by a community of dedicated musicians, sound designers, and artists from across the world.",
+      "We feel the same way about making Ableton products. The driving force behind Ableton is our passion for what we make, and the people we make it for.",
+      "Most of us are active musicians, producers, and DJs, and many of us use Live and Push every day. We come from a wide range of cultural and professional backgrounds. Some of us have PhDs, some are self-taught, and most of us are somewhere in between. What connects us is the shared belief that each of us has the skills and knowledge to contribute to something big: helping to shape the future of music culture.",
+      "Rather than having a one-size-fits-all process, we try to give our people what they need to work their magic and grow. We've learned that achieving the best results comes from building teams that are richly diverse, and thus able to explore problems from a wider set of perspectives. We don't always agree with each other, but opinion and debate are valued and openly encouraged.",
+      "We work hard to foster an environment where people can grow both personally and professionally, and we strive to create a wealth of opportunities to learn from and with each other.",
+      "Alongside an internal training program, employees are actively supported in acquiring new knowledge and skills, and coached on applying these in their daily work. In addition, staff-organized development and music salons are a chance to discuss new technologies, production techniques and best practices.",
+      "If you're joining us in Berlin, we'll help with relocation and paperwork. We'll even provide you with free German or English lessons. Plus, working in Germany means you can expect comprehensive health insurance for you and your family, as well as generous maternity and paternity leave. Office hours are flexible, but it's not all work; we have several company and team outings throughout the year as well as a variety of fun, informal small-group activities."
+
+    ]
+},
+"es": {
+  "links": [
+    "Hacemos ",
+    "Live",
+    "Push",
+    " y " ,
+    "Link"
+  ],
+  "title": [
+    ": genuinos programas y equipos hardware para la creación y la interpretación de música. Con estos productos, nuestra comunidad de usuarios crea cosas increíbles.",
+    "Hacer música no es fácil. Requiere tiempo, esfuerzo y aprendizaje. Pero cuando todo fluye, es tremendamente gratificante.",
+    "Somos más de 350 personas de 30 países distintos repartidas entre nuestra sede en Berlín y nuestras oficinas en Los Ángeles y Tokio.",
+    "Creemos que para crear instrumentos realmente excepcionales es necesario tener unos objetivos claros. Solo trabajamos en unos pocos productos y nos esmeramos para que sean geniales.",
+    "Nos apasiona lo que hacemos, pero también nos apasiona mejorar lo que somos.",
+    "Queremos que a nuestros empleados les guste estar aquí. Dado que buscamos talentos excepcionales de todo el mundo, haremos todo lo posible para que su transición sea lo más fácil posible."
+
+    ],
+  "paragraph": [
+    "Ableton se fundó en 1999 y lanzó la primera versión de Live en 2001. Nuestros productos son utilizados por una comunidad de músicos, diseñadores de sonido y artistas de todo el mundo.",
+    "Pensamos lo mismo sobre la fabricación de los productos de Ableton. Lo que nos motiva en Ableton es nuestra pasión por lo que hacemos y las personas para las que lo hacemos.",
+    "La mayoría somos músicos, productores y DJ en activo, y muchos de nosotros usamos Live and Push todos los días. Procedemos de entornos culturales y profesionales muy diversos. Algunos tenemos doctorados, otros somos autodidactas y la mayoría estamos en algún punto intermedio. Lo que nos une es la creencia compartida de que cada uno de nosotros tiene las capacidades y los conocimientos para contribuir a algo grande: ayudar a forjar el futuro de la cultura musical.",
+    "Solemos evitar los procesos de trabajo estandarizados y preferimos darle a nuestra gente lo que necesita para hacer bien su trabajo y seguir creciendo. Hemos aprendido que los mejores resultados se obtienen cuando se crean equipos muy heterogénos que son capaces de abordar los problemas desde perspectivas diveras. Aunque no siempre estemos de acuerdo, le concedemos gran importantcia al intercambio abierto de opiniones.",
+    "Hacemos todo lo posible por fomentar un entorno en el que las personas puedan desarrollarse tanto personal como profesionalmente, y nos esforzamos para crear un sinfín de oportunidades en las que aprender unos de otros.",
+    "Junto con un programa de formación interno, los empleados reciben apoyo de forma activa para adquirir nuevos conocimientos y habilidades, y se les asesora sobre cómo aplicarlos en su trabajo diario. Además, los talleres de música y desarrollo organizados por el personal son una oportunidad para hablar de nuevas tecnologías, técnicas de producción y mejores prácticas.",
+    "Si vas a trabajar con nosotros en Berlín, te ayudaremos con el traslado y los trámites. Incluso te proporcionaremos clases gratuitas de alemán o inglés. Además, trabajar en Alemania significa que podrás beneficiarte de un seguro médico completo para ti y para tu familia, así como de extensos permisos de maternidad y paternidad. Los horarios de oficina son flexibles, pero no todo es trabajo; a lo largo del año se organizan varias salidas de empresa y de los distintos equipos, además de muchas actividades informales de ocio para grupos pequeños."
+
+  ]
+}
+}
+
+export { textHome };
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,5 +1,10 @@
-function Home() {
+import { useContext } from "react";
+import { AbletonContext } from "../../components/Context";
+import { textHome } from "../../assets/text/textHome";
 
+function Home() {
+  const { language } = useContext(AbletonContext)
+  const textList = textHome[language];
   return (
   <>
     <div className="home">
@@ -9,9 +14,8 @@ function Home() {
           </div>
           <div className="page-about__text">
             <div className="body-text">
-              <h1>We make <a>Live</a> , <a>Push</a> and <a>Link</a> — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.
-              </h1>
-              <p>Ableton was founded in 1999 and released the first version of Live in 2001. Our products are used by a community of dedicated musicians, sound designers, and artists from across the world.</p>
+              <h1>{textList.links[0]}<a>{textList.links[1]}</a> , <a>{textList.links[2]}</a>{textList.links[3]}<a>{textList.links[4]}</a>{textList.title[0]}</h1>
+              <p>{textList.paragraph[0]}</p>
             </div>
           </div>
           <div className="page-about__collage">
@@ -21,8 +25,8 @@ function Home() {
           </div>
           <div className="page-agout__text has-video">
             <div className="body-text">
-                <h1>Making music isn't easy. It takes time, effort, and learning. But when you're in the flow, it's incredibly rewarding.</h1>
-                <p>We feel the same way about making Ableton products. The driving force behind Ableton is our passion for what we make, and the people we make it for.</p>
+                <h1>{textList.title[1]}</h1>
+                <p>{textList.paragraph[1]}</p>
             </div>
           </div>
           <div className="page-about__media">
@@ -30,30 +34,30 @@ function Home() {
           </div>
           <div className="page-about__text">
             <div className="body-text">
-              <h1>We are more than 350 people from 30 different countries divided between our headquarters in Berlin and our offices in Los Angeles and Tokyo.</h1>
-              <p>Most of us are active musicians, producers, and DJs, and many of us use Live and Push every day. We come from a wide range of cultural and professional backgrounds. Some of us have PhDs, some are self-taught, and most of us are somewhere in between. What connects us is the shared belief that each of us has the skills and knowledge to contribute to something big: helping to shape the future of music culture.</p>
+              <h1>{textList.title[2]}</h1>
+              <p>{textList.paragraph[2]}</p>
             </div>
           </div>
           <div className="page-about__collage"></div>
           <div className="page-about__text">
             <div className="body-text">
-              <h1>We believe it takes focus to create truly outstanding instruments. We only work on a few products and we strive to make them great.</h1>
-              <p>Rather than having a one-size-fits-all process, we try to give our people what they need to work their magic and grow. We've learned that achieving the best results comes from building teams that are richly diverse, and thus able to explore problems from a wider set of perspectives. We don't always agree with each other, but opinion and debate are valued and openly encouraged.</p>
+              <h1>{textList.title[3]}</h1>
+              <p>{textList.paragraph[3]}</p>
             </div>
           </div>
           <div className="page-about__media"></div>
           <div className="page-about__text">
             <div className="body-text">
-              <h1>We're passionate about what we do, but we're equally passionate about improving who we are.</h1>
-              <p>We work hard to foster an environment where people can grow both personally and professionally, and we strive to create a wealth of opportunities to learn from and with each other.</p>
-              <p>Alongside an internal training program, employees are actively supported in acquiring new knowledge and skills, and coached on applying these in their daily work. In addition, staff-organized development and music salons are a chance to discuss new technologies, production techniques and best practices.</p>
+              <h1>{textList.title[4]}</h1>
+              <p>{textList.paragraph[4]}</p>
+              <p>{textList.paragraph[5]}</p>
             </div>
           </div>
           <div className="page-about__collage"></div>
           <div className="page-about__text">
             <div className="body-text">
-              <h1>We want our employees to love it here. Since we're looking for exceptional talent from around the world, we will do everything we can to make your transition as easy as possible.</h1>
-              <p>If you're joining us in Berlin, we'll help with relocation and paperwork. We'll even provide you with free German or English lessons. Plus, working in Germany means you can expect comprehensive health insurance for you and your family, as well as generous maternity and paternity leave. Office hours are flexible, but it's not all work; we have several company and team outings throughout the year as well as a variety of fun, informal small-group activities.</p>
+              <h1>{textList.title[5]}</h1>
+              <p>{textList.paragraph[6]}</p>
             </div>
           </div>
           <div className="page-about__jobs-teaser"></div>
