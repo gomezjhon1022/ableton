8da5d6342c47dca4b80352c07a6fadc557bcdc3b
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-12-12 21:55:51 +0100

    Fix: route basename ableton

--- src/routes/App.js
+++ src/routes/App.js
@@ -5,8 +5,8 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <div >
+      <BrowserRouter basename='/ableton'>
         <Layout>
           <Routes>
             <Route path='/' element={<Home/>}></Route>
